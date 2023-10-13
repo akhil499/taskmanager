26493bfeedcf8f00085491e7e0827b187fbe27e8
Akhilesh K M <kmakhil294@gmail.com>
2023-10-13 18:19:19 +0530

    Updated date format checks and added update task feature

--- src/main/java/com/taskmanager/controllers/TasksController.java
+++ src/main/java/com/taskmanager/controllers/TasksController.java
@@ -2,12 +2,15 @@ package com.taskmanager.controllers;
 
 
 import com.taskmanager.dto.CreateTaskDTO;
+import com.taskmanager.dto.ErrorResponseDTO;
+import com.taskmanager.dto.UpdateTaskDTO;
 import com.taskmanager.entities.TaskEntity;
 import com.taskmanager.services.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -36,9 +39,26 @@ public class TasksController {
     }
 
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) {
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task = taskservice.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
 
+        return ResponseEntity.ok(task);
+    }
+
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e) {
+        if( e instanceof ParseException) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid date format"));
+        }
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Internal Server Error"));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable("id") int id, @RequestBody UpdateTaskDTO body) throws ParseException {
+        var task = taskservice.updateTask(id, body.getDescription(), body.getDeadline(), body.getCompleted());
+        if(task == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(task);
     }
 }
